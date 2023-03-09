f6edce7b4454358a6d4bb811feeca6efde1553b9
marcelo-souza-1999 <developermarcelosouza@gmail.com>
2023-03-08 22:40:30 -0300

    first commit

---
+++ app/src/main/java/com/marcelo/marvelheroes/presentation/ui/activitys/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.marcelo.marvelheroes.presentation.ui.activitys
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.marcelo.marvelheroes.R
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
---
+++ buildSrc/src/main/java/dependencies/BuildDependencies.kt
@@ -0,0 +1,8 @@
+package dependencies
+
+object BuildDependencies {
+
+    const val build_tools = "com.android.tools.build:gradle:${Versions.gradle}"
+    const val kotlin_gradle_plugin = "org.jetbrains.kotlin:kotlin-gradle-plugin:${Versions.kotlin}"
+    const val navigation_safe_args_plugin = "androidx.navigation:navigation-safe-args-gradle-plugin:${Versions.navigation}"
+}
