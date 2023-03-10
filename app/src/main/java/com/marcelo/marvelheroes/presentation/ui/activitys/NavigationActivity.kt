28950ef3e04db54820572bedefc71275b3b1e5b3
marcelo-souza-1999 <developermarcelosouza@gmail.com>
2023-03-09 21:27:21 -0300

    configure navHostFragment

---
+++ app/src/main/java/com/marcelo/marvelheroes/presentation/ui/activitys/NavigationActivity.kt
@@ -0,0 +1,31 @@
+package com.marcelo.marvelheroes.presentation.ui.activitys
+
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import androidx.navigation.NavController
+import androidx.navigation.fragment.NavHostFragment
+import androidx.navigation.ui.AppBarConfiguration
+import com.marcelo.marvelheroes.R
+import com.marcelo.marvelheroes.databinding.ActivityNavigationBinding.inflate
+
+class NavigationActivity : AppCompatActivity() {
+
+    private val binding by lazy { inflate(layoutInflater).apply { setContentView(root) } }
+
+    private lateinit var navController: NavController
+    private lateinit var appBarConfiguration: AppBarConfiguration
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(binding.root)
+        setupNavHost()
+    }
+    private fun setupNavHost() {
+        val navHostFragment = supportFragmentManager.findFragmentById(R.id.navHostContainer) as NavHostFragment
+
+        navController = navHostFragment.navController
+        appBarConfiguration = AppBarConfiguration(
+            setOf()
+        )
+    }
+}
