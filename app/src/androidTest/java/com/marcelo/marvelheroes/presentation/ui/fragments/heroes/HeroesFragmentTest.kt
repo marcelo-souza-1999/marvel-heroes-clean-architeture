88a7fcb46d9ed687a21b37c1b92e4c1bad979419
marcelo-souza-1999 <developermarcelosouza@gmail.com>
2023-04-16 23:00:52 -0300

    setup androidTestInstrumented

---
+++ app/src/androidTest/java/com/marcelo/marvelheroes/presentation/ui/fragments/heroes/HeroesFragmentTest.kt
@@ -0,0 +1,14 @@
+package com.marcelo.marvelheroes.presentation.ui.fragments.heroes
+
+import androidx.test.runner.AndroidJUnit4
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class HeroesFragmentTest {
+
+    @Test
+    fun shouldShowHeroes_whenViewIsCreated() {
+
+    }
+}
--- buildSrc/src/main/java/dependencies/TestDependencies.kt
+++ buildSrc/src/main/java/dependencies/TestDependencies.kt
@@ -9,6 +9,7 @@ object TestDependencies {
     const val androidx_core_testing = "androidx.arch.core:core-testing:${Versions.androidx_core_testing}"
     const val androidx_espresso = "androidx.test.espresso:espresso-contrib:${Versions.androidx_espresso}"
     const val androidx_espresso_core = "androidx.test.espresso:espresso-core:${Versions.androidx_espresso_core}"
+    const val fragment_testing = "androidx.fragment:fragment-testing:${Versions.fragment_testing}"
     const val androidx_test_runner = "androidx.test:runner:${Versions.androidx_test_runner}"
     const val androidx_test_orchestrator = "androidx.test:orchestrator:${Versions.androidx_test_orchestrator}"
     const val ok_http_mock_test = "com.squareup.okhttp3:mockwebserver:${Versions.test_mock}"
