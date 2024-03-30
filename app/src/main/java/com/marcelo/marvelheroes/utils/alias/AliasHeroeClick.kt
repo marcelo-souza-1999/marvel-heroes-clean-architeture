ac68de1572fd382053cd93a87e3d1c3082be34a5
Luiz Filipe <luiz.fmdr@gmail.com>
2024-03-30 18:39:15 -0300

    code smell

--- app/src/main/java/com/marcelo/marvelheroes/utils/alias/AliasHeroeClick.kt
+++ app/src/main/java/com/marcelo/marvelheroes/utils/alias/AliasHeroeClick.kt
@@ -4,3 +4,9 @@ import android.view.View
 import com.marcelo.marvelheroes.domain.model.HeroesViewData
 
 typealias OnHeroeItemClick = (heroes: HeroesViewData, view: View) -> Unit
+
+
+fun validateDetekt(): Int {
+    var x = 1
+    return x + 555
+}
